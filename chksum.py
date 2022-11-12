4c5b6491fc12924d3064fc3a5668c490980ee288
espehon <gregg.howlett@gmail.com>
2022-11-12 14:09:14 -0800

    added main vars and functions
    vars for storing validated input
    functions for validating input

--- chksum.py
+++ chksum.py
@@ -2,9 +2,25 @@
 
 
 
+#region: --------------------------------[ Notes ]--------------------------------
+# √   
+
+#endregion: Notes
+
+
+
+
+#region: --------------------------------[ To-Do ]--------------------------------
+# TODO: add ignore .files option
+
+#endregion: To-Do
+
+
+
 
 
 #region: --------------------------------[ Imports ]--------------------------------
+import os
 import argparse
 import checksum
 from colorama import Fore, init
@@ -21,7 +37,7 @@ parser = argparse.ArgumentParser(
     prog="CHKSUM",
     description = "Compare two checksums",
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    epilog = "If the first 2 positional arguments are checksums, the algorithm is not needed.\nExample 1: chksum ./file1 ./file2 md5\nExample 2: chksum 123456789ABCDE 123456789ABCDE",
+    epilog = "If the first 2 positional arguments are checksums, the algorithm is not needed.\n\tExample 1: chksum ./file1 ./file2 md5\n\tExample 2: chksum 123456789ABCDE 123456789ABCDE",
     add_help = False # free -h from help (-? will be used as help flag)
 )
 
@@ -33,6 +49,51 @@ parser.add_argument('position3', type=str, nargs='?', help="Checksum, file, or a
 # get args from input
 args = parser.parse_args()
 
+hashes = []
+files = []
+dirs = []
+method = None
+
 
 
 #endregion: Variables
+
+
+
+
+#region: --------------------------------[ Functions ]--------------------------------
+
+
+
+def trySetAlgorithm(value: str) -> bool:
+    global method
+    ALGORITHMS = ['md5', 'sha1', 'sha256', 'sha512']
+    if str.upper(value) in ALGORITHMS:
+        method = value
+        return True
+    return False
+
+
+def trySetFile(value: str) -> bool:
+    if os.path.isfile(value):
+        files.append(value)
+        return True
+    return False
+
+
+def trySetDir(value: str) -> bool:
+    if os.path.exists(value):
+        dirs.append(value)
+        return True
+    return False
+
+
+
+
+
+
+
+
+
+
+#endregion: Functions
